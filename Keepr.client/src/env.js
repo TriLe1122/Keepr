c188eb0abc578e7fce739fffc62b21b75bb85614
tri le <letrin93@gmail.com>
2022-12-09 13:54:28 -0700

    tri

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
+export const baseURL = dev ? 'https://keepr-3mlc.onrender.com/' : ''
 export const useSockets = false
 export const domain = 'tnl93.us.auth0.com'
 export const audience = 'https://codeworks-demos.com'
